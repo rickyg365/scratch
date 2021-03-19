f951798df968cc4042cc2ada0c28c98e1a18595b
rickyg365 <rickyg365@users.noreply.github.com>
2021-03-18 18:58:33 -0700

    updated monsters.py
    Fixed fully functional dex:
    - Uses Pokemon.csv as a database
    - Converts csv to json to sue as a reference
    - Saves a local instance of a pokedex to poke_data.json

--- pokedex/monsters.py
+++ pokedex/monsters.py
@@ -1,7 +1,9 @@
 import os
 import sys
 
+import csv
 import json
+from json.decoder import JSONDecodeError
 
 """ 
 Program: Pokedex and Digidex
@@ -13,6 +15,7 @@ Date: 03/18/2021
 class Dex:
     def __init__(self, owner, data, title="Collection"):
         self.data = data
+        self.complete_data = {}
         self.belongs = owner
         self.title = title
 
@@ -31,24 +34,56 @@ class Pokedex(Dex):
         if data is None:
             data = []
         super().__init__(ot, data,  title="Pokedex")
-        self.save_data = {}
+        self.save_file = 'poke_data.json'
+        self.database_file = 'Pokemon.csv'
+
+        # # Load instance
+        # self.load()
+
+        # Load database
+        self.load_complete(self.database_file)
+
+    def add_pokemon(self, pokemon_name):
+        poke_name = pokemon_name.capitalize()
+        poke_data = self.complete_data[poke_name]
+
+        poke_name_list = map(lambda data: data.name, self.data)
+        if poke_name not in poke_name_list:
+            print(f"[{poke_name}] added to {self.belongs}'s pokedex")
+        print(poke_data)
+        self.data.append(Pokemon(poke_name, poke_data))
+        self.save()
 
     def save(self):
+        save_data = {}
         for data in self.data:
-            self.save_data[f"{data.name}"] = {"Attribute": "Value"}
+            save_data[f"{data.name}"] = data.attr
 
-        json_obj = json.dumps(self.save_data, indent=3)
+        json_obj = json.dumps(save_data, indent=3)
 
         with open("poke_data.json", 'w') as out:
             out.write(json_obj)
 
     def load(self):
-        with open("poke_data.json", 'r') as load_file:
-            load_data = json.loads(load_file.read())
+        try:
+            with open("poke_data.json", 'r') as load_file:
+                load_data = json.loads(load_file.read())
 
-        for pokemon_name, attribute_dict in load_data.items():
-            processed_data = Pokemon(pokemon_name, attr=attribute_dict)
-            self.data.append(processed_data)
+            for pokemon_name, attribute_dict in load_data.items():
+                processed_data = Pokemon(pokemon_name, attr=attribute_dict)
+                self.data.append(processed_data)
+        except JSONDecodeError:
+            print("No Data")
+
+    def load_complete(self, csv_file):
+        self.complete_data = {}
+
+        with open(csv_file, encoding='utf-8') as csvf:
+            csvreader = csv.DictReader(csvf)
+
+            for rows in csvreader:
+                key = rows['Name']
+                self.complete_data[key] = rows
 
 
 class Digidex(Dex):
@@ -96,7 +131,7 @@ class Monster:
 
 
 class Pokemon(Monster):
-    def __init__(self, pokemon_name, poke_type="", attr=None):
+    def __init__(self, pokemon_name, attr=None):
         super().__init__(pokemon_name, attr)
         # self.type = attr['type']
         # Instance of poke is a specific pokemon not the general pokemon
@@ -104,21 +139,22 @@ class Pokemon(Monster):
 
 
 class Digimon(Monster):
-    def __init__(self, digimon_name, attr=None):
-        super().__init__(digimon_name, attr)
+    def __init__(self, digimon_id, attr=None):
+        super().__init__(digimon_id, attr)
 
 
 if __name__ == "__main__":
     # Pokedex
     p = Pokedex("RG")
-    p.load()
+    p.add_pokemon('Squirtle')
+    p.add_pokemon('Bulbasaur')
+    p.add_pokemon('Charmander')
 
-    p.save()
     print(p)
 
     # Digidex
-    d = Digidex("RG")
-    d.load()
-
-    d.save()
-    print(d)
+    # d = Digidex("RG")
+    # d.load()
+    #
+    # d.save()
+    # print(d)
