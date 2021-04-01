9b4689e29fbf6a28b329a8c269587986d15699fc
rickyg365 <rickyg365@users.noreply.github.com>
2021-04-01 14:09:42 -0700

    web

--- django-tutorial/mysite/mysite/settings.py
+++ django-tutorial/mysite/mysite/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # 'webapp',
+    'personal_site',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
