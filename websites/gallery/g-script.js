c8d31ae834f7b9a8cb661092676a072dd9f9fcb6
rickyg365 <rickyg365@users.noreply.github.com>
2021-06-22 23:24:52 -0700

    test

--- websites/gallery/g-script.js
+++ websites/gallery/g-script.js
@@ -1,3 +1,5 @@
+var menuState = "off"
+
 function changeMenu(x) {
     x.classList.toggle("change");
 }
@@ -13,6 +15,23 @@ function showPanel(x) {
         panel.style.display = "none";
     } else {
         panel.style.display = "block";
+    }
+}
+// close tab
+function closeTab() {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 }
 
@@ -36,4 +55,15 @@ function openTab(x, tabName) {
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(tabName).style.display = "block";
     x.classList.toggle("active");
+}
+
+function openMenu(x, tabName) {
+    if (menuState === "off") {
+        openTab(x, tabName);
+        menuState = "on";
+    } else {
+        closeTab();
+        menuState = "off";
+        x.classList.toggle("active");
+    }
 }
