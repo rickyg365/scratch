424cb349c7c9185920957844848be39598338ea6
rickyg365 <67453883+rickyg365@users.noreply.github.com>
2021-11-08 23:20:20 -0800

    add

---
+++ dataclasses/dataobj.py
@@ -0,0 +1,184 @@
+import os
+
+from typing import Optional
+from dataclasses import dataclass
+
+
+# from enum import Enum, auto
+
+# Enum is good for data you don't need to display, maybe just need for comparison within python
+# class GroceryType(Enum):
+#     PRODUCE = auto()
+#     DAIRY = auto()
+#     MEAT = auto()
+#     DELI = auto()
+#     BREAD = auto()
+#     PASTA = auto()
+#     CONDIMENT = auto()
+#     SPICE = auto()
+#     CANNED = auto()
+#     DRINK = auto()
+#     ALCOHOL = auto()
+#     JUNK = auto()
+
+
+@dataclass
+class GroceryObj:
+    name: str
+    type: str
+    price: float
+    brand: Optional[str] = None
+    flavor: Optional[str] = None
+    item_id: Optional[int] = None
+
+    def __str__(self):
+        self.validate_type(self.type)
+
+        return f"{self.name} \t [{self.item_id:03.0f}]: \t${self.price}  \t{self.type}"
+
+    def __repr__(self):
+        self.validate_type(self.type)
+
+        return f"<{self.name}[{self.item_id:03.0f}] : ${self.price} : {self.type}>"
+
+    def validate_type(self, check_type):
+        valid_types = [
+            "PRODUCE",
+            "DAIRY",
+            "MEAT",
+            "DELI",
+            "BREAD",
+            "PASTA",
+            "CONDIMENT",
+            "SPICE",
+            "CANNED",
+            "DRINK",
+            "ALCOHOL",
+            "JUNK"
+        ]
+
+        if check_type.upper() not in valid_types:
+            self.type = "NEW"
+
+
+class Inventory:
+    def __init__(self):
+        self.items = []
+        self.quick_access = {}
+        self.length = 0
+
+        # are we assuming items are ordered by id? in that case we can use the id as the array index num (-1 ofc)
+
+    def __str__(self):
+        text = ""
+        # text += f"#  NAME \t [_ID]: \tPRICE \tTYPE\n"
+        for _, item in enumerate(self.items):
+            text += f"{_+1}| {item}\n"
+        return text
+
+    def add_item(self, new_item: GroceryObj) -> None:
+        """ Add item to list of items """
+        self.items.append(new_item)
+        self.quick_access[new_item.item_id] = new_item
+        self.length += 1
+
+    def remove_item(self, item_id) -> bool:
+        """ Remove item using id, returns bool """
+        ''' Use Timeit to see which way is faster '''
+        # for item in self.items:
+        #     if item.item_id == item_id:
+        #         self.items.remove(item)
+        #         return True
+        # return False
+        current = self.quick_access.get(item_id, None)
+
+        if current:
+            self.items.remove(current)
+            self.quick_access.pop(item_id)
+            self.length -= 1
+            return True
+        return False
+
+    """ Modify Item??? func """
+
+    def find_item(self, item_id):
+        """ Find item by id, return None if not found """
+        current = self.quick_access.get(item_id, None)
+
+        return current
+
+
+if __name__ == "__main__":
+    # Get Terminal Dimensions
+    width, height = os.get_terminal_size()
+
+    # Sample Data
+    sample_obj = {
+        "item_id": 1,
+        "name": "carrots",
+        "type": "PRODUCE",
+        "price": 1.25
+    }
+
+    obj_list = [
+        {
+            "item_id": 1,
+            "name": "Carrots",
+            "type": "PRODUCE",
+            "price": 1.25
+        },
+        {
+            "item_id": 2,
+            "name": "Ground Beef",
+            "type": "MEAT",
+            "price": 9.24
+        },
+        {
+            "item_id": 3,
+            "name": "Milk",
+            "type": "DAIRY",
+            "price": 3.45
+        }
+    ]
+
+    # Create new blank inventory
+    my_inventory = Inventory()
+
+    # Iterate through data and add as GroceryObj to Inventory
+    for my_item in obj_list:
+        my_inventory.add_item(GroceryObj(**my_item))
+
+    # Display Current Inventory
+    print(f"\n[ Current Inventory ], Size: {my_inventory.length}")
+
+    print(f"{width * '-'}")
+    print(my_inventory)
+
+    # Edit Inventory
+    my_inventory.remove_item(2)
+
+    # Display New Inventory
+    print(f"[ Current Inventory ], Size: {my_inventory.length}")
+
+    print(f"{width * '-'}")
+    print(my_inventory)
+
+    # Item Lookup
+    print(f"[ Item Lookup ]")
+
+    print(f"{width * '-'}")
+
+    # Python anti pattern, find the best way to handle this.
+    # null_data = lambda x: "Item not found" if x is None else x
+    #
+    # print(null_data(my_inventory.find_item(3)))
+    #
+    # print(null_data(my_inventory.find_item(2)))
+
+    search = my_inventory.find_item(3)
+    print("Item not found" if search is None else search)
+
+    fail_search = my_inventory.find_item(2)
+    print("Item not found" if fail_search is None else fail_search)
+
+    print("")
---
+++ dataclasses/dataobj_test.py
@@ -0,0 +1,135 @@
+from dataobj import Inventory, GroceryObj
+
+
+class TestInventory:
+    """ Unit Test: Test component,  Integration Test: test together """
+    def __init__(self):
+        # Test that will be run during run_test method
+        self.active_test = [
+            self.add_item_test,
+            self.remove_item_test,
+            self.find_item_test
+        ]
+
+        # Reference Data
+        self.sample_grocery_obj = GroceryObj(**{
+            "item_id": 5,
+            "name": "Yerba Mate",
+            "type": "DRINK",
+            "price": 3.69
+        })
+
+        self.sample_grocery_obj_list = [
+            GroceryObj(**{
+                "item_id": 1,
+                "name": "Carrots",
+                "type": "PRODUCE",
+                "price": 1.25
+            }),
+            GroceryObj(**{
+                "item_id": 2,
+                "name": "Ground Beef",
+                "type": "MEAT",
+                "price": 9.24
+            }),
+            GroceryObj(**{
+                "item_id": 3,
+                "name": "Milk",
+                "type": "DAIRY",
+                "price": 3.45
+            })
+        ]
+
+        # Create Standard Inventory
+        self.testee = Inventory()
+
+        for test_item in self.sample_grocery_obj_list:
+            self.testee.add_item(test_item)
+
+    def run_test(self):
+        # Print Intro
+        print(f"\nRunning Test:\n")
+        for current_test in self.active_test:
+            # Reset Test Object
+            self.reset_testee()
+
+            # Run Test
+            current_test()
+            print(f"[\u2713] {current_test.__name__}: Passed")
+        print("")
+
+    def reset_testee(self):
+        """ Recreates a new default Test Inventory using provided data """
+        self.testee = Inventory()
+
+        for test_item in self.sample_grocery_obj_list:
+            self.testee.add_item(test_item)
+
+    def add_item_test(self):
+        """ Test Inventory.add_item(GroceryObj(**item_data)) method """
+        # Expected Output
+        add_item_list = [
+            *self.sample_grocery_obj_list,
+            self.sample_grocery_obj
+        ]
+
+        # Test Steps
+        self.testee.add_item(self.sample_grocery_obj)
+
+        # Test Assertion
+        assert self.testee.length == 4, f"Incorrect Length, gave {self.testee.length} instead of 4"
+        assert self.testee.items == add_item_list, \
+            f"Failed to add item:\n\nActual: \n{self.testee.items}\n\nExpected: \n{add_item_list}\n"
+
+    def remove_item_test(self):
+        """ Test Inventory.remove_item(item_id) method """
+        # Expected Output
+        remove_item_list = [
+            GroceryObj(**{
+                "item_id": 1,
+                "name": "Carrots",
+                "type": "PRODUCE",
+                "price": 1.25
+            }),
+            GroceryObj(**{
+                "item_id": 3,
+                "name": "Milk",
+                "type": "DAIRY",
+                "price": 3.45
+            })
+        ]
+
+        # Test Steps
+        self.testee.remove_item(2)
+
+        # Test Assertion
+        assert self.testee.length == 2, f"Incorrect Length, gave {self.testee.length} instead of 2"
+        assert self.testee.items == remove_item_list, \
+            f"Failed to remove item\n\nActual: \n{self.testee.items}\n\nExpected: \n{remove_item_list}\n"
+
+    def modify_item_test(self):
+        pass
+
+    def find_item_test(self):
+        # Expected Output
+        sample_output_obj = GroceryObj(**{
+                "item_id": 1,
+                "name": "Carrots",
+                "type": "PRODUCE",
+                "price": 1.25
+            })
+
+        # Test Steps
+        return_obj = self.testee.find_item(1)
+
+        # Test Assertion
+        assert return_obj == sample_output_obj, \
+            f"Failed to find item\nActual: \t{return_obj}\nExpected: \t{sample_output_obj}"
+
+
+if __name__ == "__main__":
+    # Create test object
+    test = TestInventory()
+
+    # Run Tests
+    test.run_test()
